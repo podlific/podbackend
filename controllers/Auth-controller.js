5ad134bf777a4003b3250e0b3ebb2bafefa5fbe9
tester333333 <tt2504669@gmail.com>
2022-08-22 18:49:36 +0530

    Revert "headers removed"
    This reverts commit 6c70fcb417b86d2a0b9d45093d8c05ea53b070aa.

--- controllers/Auth-controller.js
+++ controllers/Auth-controller.js
@@ -81,11 +81,15 @@ class AuthController {
     res.cookie("refreshToken", refreshToken, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
+      sameSite: "none",
+      secure: true,
     });
 
     res.cookie("accessToken", accessToken, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
+      sameSite: "none",
+      secure: true,
     });
 
     const userDto = {
@@ -139,11 +143,15 @@ class AuthController {
     res.cookie("refreshToken", refreshToken, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
+      sameSite: "none",
+      secure: true,
     });
 
     res.cookie("accessToken", accessToken, {
       maxAge: 1000 * 60 * 60 * 24 * 30,
       httpOnly: true,
+      sameSite: "none",
+      secure: true,
     });
     // response
     const userDto = {
